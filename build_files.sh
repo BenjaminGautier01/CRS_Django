68f2e9607e6e952263c514807f6a9d491f44260b
BenjaminGautier01 <benjamingautier7@gmail.com>
2024-04-28 19:46:47 +0100

    updates

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+# Ensure pip is available
+command -v pip >/dev/null 2>&1 || {
+    echo "pip is not installed. Installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3.9 get-pip.py
+}
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run Django collectstatic
+python3.9 manage.py collectstatic --noinput  # Adding --noinput to avoid any prompts
